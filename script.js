54ab12ac7dbf6b51dccf3a99db601dbf9a22bb97
Ihor Selin <ihorselin@gmail.com>
2025-02-01 23:21:59 +0200

    updated onmouse logic

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ const yesButton = document.getElementById("yes-btn");
 const popup = document.getElementById("popup");
 
 noButton.addEventListener("mouseover", () => {
+    let newX, newY;
+    const yesRect = yesButton.getBoundingClientRect();
+    const noRect = noButton.getBoundingClientRect();
+
+    do {
+        newX = Math.random() * (window.innerWidth - noRect.width);
+        newY = Math.random() * (window.innerHeight - noRect.height);
+    } while (
+        newX + noRect.width > yesRect.left && newX < yesRect.right &&
+        newY + noRect.height > yesRect.top && newY < yesRect.bottom
+    );
+
     noButton.style.position = "absolute";
-    noButton.style.left = Math.random() * 80 + "vw";
-    noButton.style.top = Math.random() * 80 + "vh";
+    noButton.style.left = `${newX}px`;
+    noButton.style.top = `${newY}px`;
 });
 
 yesButton.addEventListener("click", () => {
     popup.style.display = "flex";
 });
-
-// Hide the popup initially
-popup.style.display = "none";
