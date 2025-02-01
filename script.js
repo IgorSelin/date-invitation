a4356801daaf1fcef8587ae3d7a25108918c40f4
Ihor Selin <ihorselin@gmail.com>
2025-02-02 00:16:11 +0200

    added fixed font styles

--- script.js
+++ script.js
@@ -3,23 +3,15 @@ const yesButton = document.getElementById("yes-btn");
 const popup = document.getElementById("popup");
 
 noButton.addEventListener("mouseover", () => {
-    let newX, newY;
-    const yesRect = yesButton.getBoundingClientRect();
-    const noRect = noButton.getBoundingClientRect();
-
-    do {
-        newX = Math.random() * (window.innerWidth - noRect.width);
-        newY = Math.random() * (window.innerHeight - noRect.height);
-    } while (
-        newX + noRect.width > yesRect.left && newX < yesRect.right &&
-        newY + noRect.height > yesRect.top && newY < yesRect.bottom
-    );
-
     noButton.style.position = "absolute";
-    noButton.style.left = `${newX}px`;
-    noButton.style.top = `${newY}px`;
+    noButton.style.left = Math.random() * 80 + "vw";
+    noButton.style.top = Math.random() * 80 + "vh";
 });
 
 yesButton.addEventListener("click", () => {
-    popup.style.display = "flex";
+    popup.style.display = "block";
+});
+
+popup.addEventListener("click", () => {
+    popup.style.display = "none";
 });
